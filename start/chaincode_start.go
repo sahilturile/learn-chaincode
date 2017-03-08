44fa4c882847d8b85cb701fe7d022d5b8862a99f
sahilturile <sahilturile@in.ibm.com>
2017-03-08 13:10:48 +0530

    My changes per tutorial

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -42,6 +42,11 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
+	err:= stub.PutState("hello_world", []byte(args[0]))
+	if err!=nil {
+	return nil, err
+	}
+	
 
 	return nil, nil
 }
@@ -53,7 +58,10 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	// Handle different functions
 	if function == "init" {													//initialize the chaincode state, used as reset
 		return t.Init(stub, "init", args)
+	} else if function == "write" {
+	return t.write(stub, args)
 	}
+	
 	fmt.Println("invoke did not find func: " + function)					//error
 
 	return nil, errors.New("Received unknown function invocation: " + function)
@@ -68,7 +76,50 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 		fmt.Println("hi there " + function)						//error
 		return nil, nil;
 	}
+	if function == "read"{
+		return t.read(stub, args)
+	}
+	
 	fmt.Println("query did not find func: " + function)						//error
 
 	return nil, errors.New("Received unknown function query: " + function)
+}
+
+func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var name, value string
+	var err error
+	fmt.Println("running write()")
+	
+	if len(args) !=2 {
+	return nil, errors.New("Incorrect number of Arguments. Expecting 2. Name of Variable and value of set")
+	}
+	
+	name = args[0]
+	value = args[1]
+	err = stub.PutState(name, []byte(value))
+
+	if err != nil {
+	return nil, err
+	}
+	return nil, nil
+}
+
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var name, jsonResp string
+	var err error
+
+	if len(args) !=1 {
+		return nil, errors.New("Incorrect Number of arguments. Expecting 1. Name of the var to query")
+	}
+	
+	name = args[0]
+	valAsbytes, err := stub.GetState(name)
+	
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + name + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+	
+	return valAsbytes, nil
+	
 }
